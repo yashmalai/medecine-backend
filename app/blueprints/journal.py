d0e69c6529fa4810393ce9a0973235edfa96ffcc
yashmalai <ilnaz204@yandex.ru>
2024-08-29 18:36:54 +0500

    fix journal get

--- app/blueprints/journal.py
+++ app/blueprints/journal.py
@@ -8,5 +8,5 @@ journal_bp = Blueprint('journal_bp', __name__)
 @journal_bp.route('/log', methods=['GET'])
 def get_logs():
     logs = journal.query.all()
-    logs_list = [{'id': log.id, 'action_type': log.action_type, 'action_id': log.action_id} for log in logs]
+    logs_list = [{'id': log.id, 'action_type': log.action_type, 'action_id': log.action_id, 'created_at': log.created_at} for log in logs]
     return jsonify(logs_list)
