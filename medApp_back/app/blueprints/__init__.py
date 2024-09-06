7803a258e925b5a2bbd0e577a113416a42e4fa63
pd281101 <stalkercoo@yandex.ru>
2024-09-06 18:37:05 +0500

    Delete medApp_back directory

--- medApp_back/app/blueprints/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from . import journal, medicine, training
-
-journal_bp = Blueprint('journal_bp', __name__)
-medicine_bp = Blueprint('medicine_bp', __name__)
-training_bp = Blueprint('training_bp', __name__)
